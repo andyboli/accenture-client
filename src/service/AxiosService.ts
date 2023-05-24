3a0794db17cb73f1f1233332da90d60f16e8a838
andyboli <webdevelopmentbolivar@gmail.com>
2023-05-24 03:49:52 -0300

    fix getCep url

--- src/service/AxiosService.ts
+++ src/service/AxiosService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getCEP = async (cep: string): Promise<{ data: { cep: string }[] }> =>
-  axios.get(`/api/${cep}`, {
+  axios.get(`/api/?cep=${cep}`, {
     headers: {
       Accept: "application/json",
     },
